8af08d340591a173592d2497b8d5e37594eb87ef
jaephillseo <jaephillseo@gmail.com>
2018-12-03 13:26:28 +0700

    Fixed UI, added ImageLink form and buttons, Logo also added(temporary)

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import soybean from './soybean.png'
+import './Logo.css'
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className=" br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
- 				<div className="Tilt-inner"> 👽 </div>
+ 				<div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='logo' src={soybean}/> </div>
 			</Tilt>
 		</div>
 	);

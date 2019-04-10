58c831ed757735bf2045290207988714d96c3076
jaephillseo <jaephillseo@gmail.com>
2019-04-11 00:04:49 +0700

    Changed logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import soybean from './soybean.png'
+import red from './red.png'
 import './Logo.css'
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className=" br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
- 				<div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='logo' src={soybean}/> </div>
+ 				<div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='logo' src={red}/> </div>
 			</Tilt>
 		</div>
 	);
